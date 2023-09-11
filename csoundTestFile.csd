6a3b2df629ee4bc6a4922c388508ae8601f0f756
Gwynn Artis <gwynn.artis@gmail.com>
2023-09-11 20:37:22 +0100

    Added Csound and used 'Stria' example

---
+++ csoundTestFile.csd
@@ -0,0 +1,41 @@
+<CsoundSynthesizer>
+<CsOptions>
+-odac -d
+</CsOptions>
+<CsInstruments>
+
+sr = 44100
+ksmps = 32
+nchnls = 2
+0dbfs = 1
+
+instr 1
+	asig oscili p4, p5
+	aenv linseg 0, 0.5, 1, 0.5, 1, 0.5, 0.5, 0.5, 0
+	aout = asig*aenv
+	out aout, aout
+endin
+schedule(1, 1, 2, 0.5, 440)
+
+</CsInstruments>
+<CsScore>
+
+</CsScore>
+</CsoundSynthesizer>
+<bsbPanel>
+ <label>Widgets</label>
+ <objectName/>
+ <x>100</x>
+ <y>100</y>
+ <width>320</width>
+ <height>240</height>
+ <visible>true</visible>
+ <uuid/>
+ <bgcolor mode="nobackground">
+  <r>255</r>
+  <g>255</g>
+  <b>255</b>
+ </bgcolor>
+</bsbPanel>
+<bsbPresets>
+</bsbPresets>
---
+++ csound_linker.js
@@ -0,0 +1,72 @@
+const csoundjs = "./csound.js";
+let csound = null;
+
+let isOn = false;
+const csd = './csoundTestFile.csd'
+
+async function start() {
+    if (csound == null) {
+        const { Csound } = await import(csoundjs);
+        csound = await Csound();
+        await copyUrlToLocal(csd, csd) // are there not supposed to be semicolons here?
+        await csound.compileCsd(csd)
+        await csound.on("message", handleMessage); // handles csound messages - will handle later
+        await csound.start();
+        isOn = true;
+    }
+    if (!isOn) {
+        await csound.resume();
+        isOn = true;
+    }
+}
+
+let count = 0;
+function handleMessage(message) {
+    let element = document.getElementById('console');
+    element.innerHTML += message + '\n';
+    element.scrollTop = 99999;
+    if (count == 1000) {
+        count = 0;
+        element.innerHTML == "";
+    }
+    count += 1;
+};
+
+async function copyUrlToLocal(src, dest) {
+    // fetch the file
+    let srcfile = await fetch(src)
+    // get the file data as an array
+    let dat = await srcfile.arrayBuffer();
+    // write the data as a new file in the filesystem
+    await csound.fs.writeFile(dest, new Uint8Array(dat));
+}
+
+async function pause() {
+    if (csound != null) {
+        if (isOn) {
+            await csound.pause();
+            isOn = false;
+        } else {
+            await csound.resume();
+            isOn = true;
+        }
+    }
+}
+
+// const code = `
+// instr 1
+//     out linenr(oscili(p4, p5), 0.01, 0.5, 0.01)
+// endin
+// schedule(1, 0, 1, 0.5, 440)
+// `;
+
+// async function play() {
+//     if(csound == null) {
+//         const {Csound} = await import(csoundjs);
+//         csound = await Csound();
+//         await csound.setOption("-odac");
+//         await csound.compileOrc(code);
+//         await csound.start();
+//     }
+//     else await csound.inputMessage(`i1 0 1 0.2 440`);
+// }
